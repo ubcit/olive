a0974529da9fabf88787f84c934bed431feacb65
Hussein Abd-Ali <62807973+7u01@users.noreply.github.com>
2023-06-06 22:10:06 +0300

    olive

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 12, 10, 8)}); // year, month, date, hour
+        });
+});		
+
